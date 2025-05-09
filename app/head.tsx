c1ede0fa42230614c22ac5e1c9dc2a3bd30546a7
SebRVV <112523636+SebSRVV@users.noreply.github.com>
2025-05-08 22:33:43 -0500

    head

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+    return (
+      <>
+        <title>SafePass&Docs</title>
+        <meta name="description" content="App para cifrado y contraseñas" />
+        <link rel="icon" href="/favicon.ico" />
+      </>
+    );
+  }
